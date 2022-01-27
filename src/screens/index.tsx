7001359d2f5ab432687c9ec17cacc4902e41036f
thiago <thiagogcamp@gmail.com>
2022-01-27 15:03:15 -0300

    feat(src): added necessary configs

---
+++ src/screens/index.tsx
@@ -0,0 +1,6 @@
+import HomeScreen from "./Home";
+import SearchScreen from "./Search";
+import SettingsScreen from "./Settings";
+import CityScreen from "./City";
+
+export { HomeScreen, SearchScreen, CityScreen, SettingsScreen };
