af91eaa7418a6419ce980ff286ff20143165ca75
thiago <thiagogcamp@gmail.com>
2022-01-28 01:04:33 -0300

    feat(routes): added a header in city page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,18 +3,61 @@ import { createStackNavigator } from "@react-navigation/stack";
 import BottomTabNavigator from "./BottomTabNavigator";
 import type { RootStackParamList } from "./routes.types";
 import { CityScreen } from "../screens";
+import { Button, Icon } from "@ui-kitten/components";
 
 const { Navigator, Screen } = createStackNavigator<RootStackParamList>();
 
 export default (): ReactElement => {
   return (
-    <Navigator
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Screen name='BottomTabNavigator' component={BottomTabNavigator} />
-      <Screen name='City' component={CityScreen} />
+    <Navigator>
+      <Screen
+        name='BottomTabNavigator'
+        component={BottomTabNavigator}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Screen
+        name='City'
+        component={CityScreen}
+        options={{
+          headerTitle: "Barcelona",
+          headerTitleStyle: {
+            color: "#fff",
+          },
+          headerStyle: {
+            backgroundColor: "#0f0f0f",
+            shadowColor: "transparent",
+            borderColor: "transparent",
+          },
+          headerRight: () => (
+            <Button
+              onPress={() => alert("This is a button!")}
+              style={{ backgroundColor: "#0f0f0f", borderColor: "transparent" }}
+            >
+              <Icon
+                fill='#fff'
+                height='32px'
+                width='24px'
+                name='heart-outline'
+              />
+            </Button>
+          ),
+          headerLeft: () => (
+            <Button
+              onPress={() => alert("This is a button!")}
+              style={{ backgroundColor: "#0f0f0f", borderColor: "transparent" }}
+            >
+              <Icon
+                fill='#fff'
+                height='32px'
+                width='24px'
+                name='arrow-back-outline'
+              />
+            </Button>
+          ),
+        }}
+      />
     </Navigator>
   );
 };
