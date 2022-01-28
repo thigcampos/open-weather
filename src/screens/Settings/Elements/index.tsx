fb5994c6343d317731b16ad00ecd237ece85b8b1
thiago <thiagogcamp@gmail.com>
2022-01-28 01:01:35 -0300

    feat(screens): created settings options

---
+++ src/screens/Settings/Elements/index.tsx
@@ -0,0 +1,45 @@
+import { ReactElement, useState } from "react";
+import { Layout, Text, Toggle } from "@ui-kitten/components";
+
+type Props = {
+  subtitle: string;
+  title: string;
+};
+
+export default function Config({ subtitle, title }: Props): ReactElement {
+  const [checked, setChecked] = useState(false);
+
+  const onCheckedChange = (isChecked: boolean) => {
+    setChecked(isChecked);
+  };
+
+  return (
+    <Layout style={{ width: "100%", marginBottom: 24 }}>
+      <Text
+        style={{
+          fontSize: 12,
+          fontWeight: "bold",
+          color: "#C0C0C0",
+          marginBottom: 12,
+        }}
+      >
+        {subtitle}
+      </Text>
+      <Layout
+        style={{
+          backgroundColor: "#202020",
+          borderRadius: 14,
+          width: "100%",
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          padding: 16,
+        }}
+      >
+        <Text>{title}</Text>
+        <Toggle checked={checked} onChange={onCheckedChange} />
+      </Layout>
+    </Layout>
+  );
+}
--- src/screens/Settings/index.tsx
+++ src/screens/Settings/index.tsx
@@ -1,10 +1,22 @@
 import * as React from "react";
 import { Layout, Text } from "@ui-kitten/components";
+import { SafeAreaView } from "react-native";
+import Config from "./Elements";
 
 export default function SettingsScreen() {
   return (
-    <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Settings!</Text>
-    </Layout>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Layout
+        style={{
+          flex: 1,
+          padding: 24,
+          alignItems: "center",
+          justifyContent: "flex-start",
+        }}
+      >
+        <Config subtitle='Tema do aplicativo' title='Modo Escuro' />
+        <Config subtitle='Medida de Temperatura' title='Sistema Imperial' />
+      </Layout>
+    </SafeAreaView>
   );
 }
