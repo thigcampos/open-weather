d01589afe5ead87b7f0ba0b2532290846eb5937e
thiago <thiagogcamp@gmail.com>
2022-01-29 00:44:32 -0300

    feat(screens): added custom header

--- src/screens/Settings/index.tsx
+++ src/screens/Settings/index.tsx
@@ -9,11 +9,27 @@ export default function SettingsScreen() {
       <Layout
         style={{
           flex: 1,
-          padding: 24,
+          padding: 16,
           alignItems: "center",
           justifyContent: "flex-start",
         }}
       >
+        <Layout
+          style={{
+            width: "100%",
+            height: 40,
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginTop: 40,
+            marginBottom: 8,
+          }}
+        >
+          <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+            Configurações
+          </Text>
+        </Layout>
         <Config subtitle='Tema do aplicativo' title='Modo Escuro' />
         <Config subtitle='Medida de Temperatura' title='Sistema Imperial' />
       </Layout>
