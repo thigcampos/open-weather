3a3e4b97bc5d5a2aed4185b2d6f4413f760bcc74
thiago <thiagogcamp@gmail.com>
2022-01-29 00:44:47 -0300

    feat(screens): added custom header

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -1,18 +1,47 @@
 import * as React from "react";
 import { SafeAreaView } from "react-native";
-import { Layout, Input } from "@ui-kitten/components";
+import { Layout, Input, Text } from "@ui-kitten/components";
+import { ReactElement } from "react";
+import {
+  BottomTabNavigatorParamList,
+  RootStackParamList,
+} from "../../routes/routes.types";
+import { StackScreenProps } from "@react-navigation/stack";
 
-export default function SearchScreen() {
+type SearchScreenProps = StackScreenProps<
+  BottomTabNavigatorParamList,
+  "Search"
+> &
+  StackScreenProps<RootStackParamList>;
+
+export default ({
+  navigation: { navigate },
+}: SearchScreenProps): ReactElement => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Layout
         style={{
           flex: 1,
-          padding: 24,
+          paddingLeft: 16,
+          paddingRight: 16,
           alignItems: "center",
           justifyContent: "flex-start",
         }}
       >
+        <Layout
+          style={{
+            width: "100%",
+            height: 40,
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginTop: 40,
+            marginBottom: 8,
+          }}
+        >
+          <Text style={{ fontSize: 20, fontWeight: "bold" }}>Buscar</Text>
+        </Layout>
         <Input
           style={{ borderRadius: 14 }}
           placeholder='Pesquise uma cidade'
@@ -21,4 +50,4 @@ export default function SearchScreen() {
       </Layout>
     </SafeAreaView>
   );
-}
+};
