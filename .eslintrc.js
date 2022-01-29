d0c5197c9f3a76fd9b25b74a64db0fd3760a4e0c
thiago <thiagogcamp@gmail.com>
2022-01-29 00:47:56 -0300

    refactor(app): updated necessary root files

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  settings: {
+    react: {
+      version: "latest",
+    },
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [1, { extensions: [".tsx"] }],
+    "import/extensions": "off",
+    "react/jsx-props-no-spreading": "off",
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": ["error"],
+    "import/no-unresolved": "off",
+    "react/jsx-fragments": "off",
+    "react/no-array-index-key": "off",
+  },
+};
