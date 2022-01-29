60f15dceda8ad292671219e642f894d7b3cb7971
thiago <thiagogcamp@gmail.com>
2022-01-29 00:40:54 -0300

    feat(utils): added a fake list of favorites to test the app

---
+++ src/utils/fakeList/index.ts
@@ -0,0 +1,33 @@
+type CityType = {
+  title: string;
+  latitude: number;
+  longitude: number;
+};
+
+export const Cities: Array<CityType> = [
+  {
+    title: "Barcelona",
+    latitude: 41.3850639,
+    longitude: 2.1734035,
+  },
+  {
+    title: "Milão",
+    latitude: 45.4654219,
+    longitude: 9.1859243,
+  },
+  {
+    title: "Chicago",
+    latitude: 41.8781136,
+    longitude: -87.6297982,
+  },
+  {
+    title: "Marseille",
+    latitude: 43.296482,
+    longitude: 5.36978,
+  },
+  {
+    title: "Bilbao",
+    latitude: 43.2630126,
+    longitude: -2.9349852,
+  },
+];
